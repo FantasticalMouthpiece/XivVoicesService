cc25e15ad27d0ec3e7c7446187c0a43bfe738c9f
FantasticalMouthpiece <fantasticalmouthpiece@gmail.com>
2025-01-27 12:02:41 -0800

    chore: set db connection info to same .env used by docker postgres image

--- XivVoicesService/Program.cs
+++ XivVoicesService/Program.cs
@@ -1,5 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using XivVoicesService;
 using XivVoicesService.Models;
+
+// Load .env
+var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory())?.FullName;
+if (projectRoot != null) {
+    DotEnv.Load(Path.Combine(projectRoot, ".env"));
+}
+
+// Config
+var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +20,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// DB Connection Info
+var dbHost = config["POSTGRES_HOST"] ?? throw new ArgumentNullException("config[\"POSTGRES_HOST\"]");
+var dbPort = config["POSTGRES_PORT"] ?? throw new ArgumentNullException("config[\"POSTGRES_PORT\"]");
+var dbName = config["POSTGRES_DB"] ?? throw new ArgumentNullException("config[\"POSTGRES_DB\"]");
+var dbUsername = config["POSTGRES_USER"] ?? throw new ArgumentNullException("config[\"POSTGRES_USER\"]");
+var dbPassword = config["POSTGRES_PASSWORD"] ?? throw new ArgumentNullException("config[\"POSTGRES_PASSWORD\"]");
+
 // Add db contexts
 builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options => options.UseNpgsql($"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUsername};Password={dbPassword}")
 );
 
 // Add repositories
